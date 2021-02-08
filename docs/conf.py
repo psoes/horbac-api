4070e12df18bff12d09afb38c1e8440c68d37f5f
tsognong <tsognong.fidele@gmail.com>
2021-02-08 11:00:49 +0100

    docs themes modified

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
-extensions = ['recommonmark']
+extensions = ['sphinx_rtd_theme', 'recommonmark']
 # -- Project information -----------------------------------------------------
 
 project = 'horbac idp'
@@ -47,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
